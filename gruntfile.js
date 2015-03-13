02faebf2fd41e9a75630af9fc8ee3f58031508ae
Terry <terry@somday.com>
2015-03-13 12:40:58 +0800

    remove open in grunt

--- gruntfile.js
+++ gruntfile.js
@@ -5,20 +5,10 @@ grunt.initConfig({
         dev: {
             script: 'bin/www'
         }
-    },
-    open: {
-        delayed: {
-            path: 'http://localhost:8337',
-            app: 'Google Chrome',
-            options: {
-                openOn: 'serverListening'
-            }
-        }
     }
 });
     // load nodemon
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-open');
-    grunt.registerTask('default',['nodemon', 'open'])
+    grunt.registerTask('default',['nodemon'])
 }
